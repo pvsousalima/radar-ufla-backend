cf5a40ab30e77eaf74d5edde4c778fb7704ee9c5
Pedro Victor <pvsousalima@gmail.com>
2017-03-28 15:58:50 -0300

    adicionando o endpoint /voto

--- models/index.js
+++ models/index.js
@@ -13,13 +13,13 @@ var options = {
 }
 
 // configuracao de testes ou de desenvolvimento para acessar o mongodb
-// if (process.env.NODE_ENV === 'develop') {
+if (process.env.NODE_ENV === 'develop') {
     mongoose.connect('mongodb://localhost/test', options);
     console.log('Connected to mongoDB database on localhost.');
-// } else {
-//     mongoose.connect('mongodb://admin:radar@ds147079.mlab.com:47079/radarufladb', options)
-//     console.log('Connected to mongoDB database on localhost.');
-// }
+} else {
+    mongoose.connect('mongodb://admin:radar@ds147079.mlab.com:47079/radarufladb', options)
+    console.log('Connected to mongoDB database on localhost.');
+}
 
 
 // Models to access the collection on database
